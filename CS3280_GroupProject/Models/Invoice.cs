e63ee3f5e48a9314c4edebea9769abf055d9272f
Kayden Miller <kaydenmiller1@gmail.com>
2018-10-31 19:01:20 -0600

    This Commit Will: + Add an Invoice DTO + Add a ViewModel DTO for the Search Window

---
+++ CS3280_GroupProject/Models/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280_GroupProject.Models
+{
+    /// <summary>
+    /// This is all information that will represent an Invoice Record in
+    /// the Database.
+    /// </summary>
+    public class Invoice
+    {
+    }
+}
---
+++ CS3280_GroupProject/Search/ViewModel/SearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280_GroupProject.Search.ViewModel
+{
+    /// <summary>
+    /// This object stores the required information to be displayed
+    /// onto the Search Window.
+    /// </summary>
+    public class SearchViewModel
+    {
+    }
+}
