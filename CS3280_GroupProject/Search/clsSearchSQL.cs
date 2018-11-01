cde7c177eab7f07b3d774b8ac8fbccce7a0ca1ab
Kayden Miller <kaydenmiller1@gmail.com>
2018-10-31 18:20:52 -0600

    This commit will: + Add a test method to SearchSQL class

--- CS3280_GroupProject/Search/clsSearchSQL.cs
+++ CS3280_GroupProject/Search/clsSearchSQL.cs
@@ -8,6 +8,10 @@ namespace CS3280_GroupProject.Search
 {
     public class clsSearchSQL
     {
-
+        public string SearchSQLTestMethod(string sInvoiceID)
+        {
+            string SQL = $"SELECT * FROM Invoices WHERE InvoiceNum = {sInvoiceID};";
+            return SQL;
+        }
     }
 }
