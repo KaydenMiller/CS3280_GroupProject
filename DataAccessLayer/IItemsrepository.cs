0df9975f47cc60eb9a5c30df41eb08d817354014
Kayden Miller <kaydenmiller1@gmail.com>
2018-11-20 08:23:12 -0700

    Create the ItemsRepository Interface

---
+++ DataAccessLayer/IItemsrepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+namespace DataAccessLayer
+{
+    /// <summary>
+    /// Interface to represent the contract for specialized Items Repository Methods
+    /// </summary>
+    public interface IItemsRepository
+    {
+        /// <summary>
+        /// Will retrieve any item with the EXACT description entered.
+        /// </summary>
+        /// <param name="desc"></param>
+        /// <returns></returns>
+        IEnumerable<Item> GetItemByDescription(string desc);
+
+        /// <summary>
+        /// Will retrieve any item with the cost entered
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        IEnumerable<Item> GetItemByExactCost(float cost);
+    }
+}
