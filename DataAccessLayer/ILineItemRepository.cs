19c177bf22e8b6780598e3846b890c9686b4004a
Kayden Miller <kaydenmiller1@gmail.com>
2018-11-20 08:39:46 -0700

    Add LineItem Repository Interface

---
+++ DataAccessLayer/ILineItemRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+namespace DataAccessLayer
+{
+    /// <summary>
+    /// LineItem Repository
+    /// </summary>
+    public interface ILineItemRepository
+    {
+        /// <summary>
+        /// Will retrieve all line items that contain the item code
+        /// </summary>
+        /// <param name="ItemCode"></param>
+        /// <returns></returns>
+        IEnumerable<LineItem> GetLineItemsByItemCode(string ItemCode);
+
+        /// <summary>
+        /// Will retrieve all line items in this line position
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        IEnumerable<LineItem> GetLineItemsByLineNumber(int line);
+    }
+}
